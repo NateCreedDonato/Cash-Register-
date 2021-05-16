79d154b607c9260b0a52b3014cdc4d218de5bf65
Nathaniel Donato <nathanieldonatofx@gmail.com>
2021-05-16 13:00:23 +0200

    Formatting Done

--- cash-register.js
+++ cash-register.js
@@ -1,5 +1,13 @@
 var money = [
-  { name: "ONE HUNDRED", val: 100.0 }, { name: "TWENTY", val: 20.0 }, { name: "TEN", val: 10.0 }, { name: "FIVE", val: 5.0 }, { name: "ONE", val: 1.0 }, { name: "QUARTER", val: 0.25 }, { name: "DIME", val: 0.1 }, { name: "NICKEL", val: 0.05 }, { name: "PENNY", val: 0.01 }
+  { name: "ONE HUNDRED", val: 100.0 },
+  { name: "TWENTY", val: 20.0 },
+  { name: "TEN", val: 10.0 },
+  { name: "FIVE", val: 5.0 },
+  { name: "ONE", val: 1.0 },
+  { name: "QUARTER", val: 0.25 },
+  { name: "DIME", val: 0.1 },
+  { name: "NICKEL", val: 0.05 },
+  { name: "PENNY", val: 0.01 },
 ];
 
 function checkCashRegister(price, cash, cid) {
@@ -7,7 +15,7 @@ function checkCashRegister(price, cash, cid) {
   var change = cash - price;
 
   var register = cid.reduce(
-    function(acc, curr) {
+    function (acc, curr) {
       acc.total += curr[1];
       acc[curr[0]] = curr[1];
       return acc;
@@ -26,9 +34,9 @@ function checkCashRegister(price, cash, cid) {
     return output;
   }
 
-  var change_arr = money.reduce(function(acc, curr) {
+  var change_arr = money.reduce(function (acc, curr) {
     var value = 0;
-    
+
     while (register[curr.name] > 0 && change >= curr.val) {
       change -= curr.val;
       register[curr.name] -= curr.val;
@@ -36,11 +44,11 @@ function checkCashRegister(price, cash, cid) {
 
       change = Math.round(change * 100) / 100;
     }
-    
+
     if (value > 0) {
       acc.push([curr.name, value]);
     }
-    return acc; 
+    return acc;
   }, []);
 
   if (change_arr.length < 1 || change > 0) {
@@ -54,5 +62,13 @@ function checkCashRegister(price, cash, cid) {
 }
 
 checkCashRegister(19.5, 20.0, [
-  ["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90.0], ["FIVE", 55.0], ["TEN", 20.0], ["TWENTY", 60.0], ["ONE HUNDRED", 100.0]
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90.0],
+  ["FIVE", 55.0],
+  ["TEN", 20.0],
+  ["TWENTY", 60.0],
+  ["ONE HUNDRED", 100.0],
 ]);
---
+++ readme.md
@@ -0,0 +1,3 @@
+THis is the readme for this code.
+
+The function on this code is to act as a cash register that is used in shops all around the world.
